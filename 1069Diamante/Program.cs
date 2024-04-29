83782242487b2c94473a5251c59a6a932bb2aa15
MathewsH <mathewscosta12@gmail.com>
2024-04-29 19:13:24 -0400

    Adicionar arquivos de projeto.

---
+++ 1069Diamante/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1069Diamante
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int j = 0; j < n; j++)
+            {
+                int cont = 0;
+                char[] texto = Console.ReadLine().ToCharArray();
+
+                for (int i = 0; i < texto.Length; i++)
+                {
+                    if ('<' == texto[i])
+                    {
+                        for (int l = i; l < texto.Length; l++)
+                        {
+                            if('>'== texto[l])
+                            {
+                                cont++;
+                                texto[l] = '.';
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                Console.WriteLine(cont);
+                
+            }
+            Console.ReadLine();
+        }
+    }
+}
